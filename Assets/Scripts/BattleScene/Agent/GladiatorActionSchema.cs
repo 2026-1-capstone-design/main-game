5c8728918fbed3c8acf8698a0262deed89b11f3c
kurateh <me@kurateh.com>
2026-04-27 11:26:22 +0900

    feat: apply poca and advance reward and action logic

--- Assets/Scripts/BattleScene/Agent/GladiatorActionSchema.cs
+++ Assets/Scripts/BattleScene/Agent/GladiatorActionSchema.cs
@@ -1,10 +1,22 @@
 public static class GladiatorActionSchema
 {
-    public const int Idle = 0;
-    public const int Forward = 1;
-    public const int Retreat = 2;
-    public const int AttackStart = 3;
-    public const int AttackSlotCount = BattleTeamConstants.MaxUnitsPerTeam;
-    public const int MoveAttackBranchSize = AttackStart + AttackSlotCount;
+    public const int IntentMove = 0;
+    public const int IntentAttack = 1;
+    public const int IntentHold = 2;
+    public const int IntentBranchSize = 3;
+
+    public const int MoveNone = 0;
+    public const int MoveForward = 1;
+    public const int MoveBackward = 2;
+    public const int MoveStrafeLeft = 3;
+    public const int MoveStrafeRight = 4;
+    public const int MoveKeepRange = 5;
+    public const int MoveBranchSize = 6;
+
+    public const int TargetBranchSize = BattleTeamConstants.MaxUnitsPerTeam;
+
+    public const int RotateNone = 0;
+    public const int RotateLeft = 1;
+    public const int RotateRight = 2;
     public const int RotationBranchSize = 3;
 }
