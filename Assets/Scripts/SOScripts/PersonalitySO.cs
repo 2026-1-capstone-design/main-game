111beb91cb65d0852363d29764bf543a855bcf77
rlawlsgurzzz <rlawlsgurzzz@naver.com>
2026-05-26 22:57:18 +0900

    fix: format

--- Assets/Scripts/SOScripts/PersonalitySO.cs
+++ Assets/Scripts/SOScripts/PersonalitySO.cs
@@ -15,15 +15,9 @@ public sealed class PersonalitySO : ScriptableObject
     public int speechStyle = 0;
 
     [Header("SOT Command Obedience")]
-    public int[] obedienceRates =
-    {
-        90, 80, 80, 80, 90, 80, 95, 90
-    };
+    public int[] obedienceRates = { 90, 80, 80, 80, 90, 80, 95, 90 };
 
-    public int[] fallbackWeights =
-    {
-        30, 20, 30, 20, 100, 30, 5, 0
-    };
+    public int[] fallbackWeights = { 30, 20, 30, 20, 100, 30, 5, 0 };
 
     // 그냥 참고용: 순응율 총합. 대체로 650 이상 유지가 바람직해보이고, 0~4(공격과 이동 4개)는 대체로 높게 잡는것이 바람직해보임
     // 산만함/반항적과 같은 "명백하게 명령을 잘 듣지 않는 성격"의 경우에는 총합 floor를 500까지 내려가게 둬도 될 듯합니다
